4dcbc616417b4f7fb422096cf52d76a0d4481af5
ericksa1 <132090323+ericksa1@users.noreply.github.com>
2024-10-07 15:28:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+body {
+    font-family: 'Arial', sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #e4d4c8; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.container {
+    max-width: 800px;
+    margin: 0;
+    padding: 20px;
+    background-color: #d0b49f;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    text-align: center;
+}
+
+h1 {
+    color: #523a28;
+}
+
+form {
+    margin-bottom: 20px;
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+th, td {
+    padding: 10px;
+    border: 1px solid #a47551; 
+    text-align: left;
+}
+
+th {
+    background-color: #d0b49f;
+    color: #523a28; 
+}
+
+td {
+    background-color: #ffffff; 
+}
 
